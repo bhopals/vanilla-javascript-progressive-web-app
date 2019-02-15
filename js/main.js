0049c67e4e2c6c9047dac126fa9fd7768e37155f
Bhopal Singh <bhopalsinghsis@gmail.com>
2019-02-15 17:09:34 +0400

    Web App banner Demo

--- js/main.js
+++ js/main.js
@@ -11,8 +11,10 @@ function toggleNav(){
     }
     hidden = !hidden;
 }
+var pwaSupport = false;
 
 if('serviceWorker' in navigator){
+    pwaSupport = true;
     //register the service worker
     navigator.serviceWorker.register('/sw.js').then(function(result){
         console.log('Service Worker Registered');
@@ -52,4 +54,22 @@ window.addEventListener('appinstalled', function(evt){
     console.log('App Installed Event');
 });
 
+window.onload = function(){
+    if(pwaSupport){
+        var p = navigator.platform;
+        if(p === 'iPhone' || p === 'iPad' || p === 'iPod'){
+            if(!navigator.standalone){
+                var lastShown = parseInt(localStorage.getItem('lastShown'));
+                var now = new Date().getTime();
+                if(isNaN(lastShown) || (lastShown + 1000*60*60*24*7) <= now){
+                    document.getElementById('instructions').style.display = 'block';
+                    localStorage.setItem('lastShown', now);
+                }
+            }
+        }
+    }
+};
 
+function hideInstructions(){
+    document.getElementById('instructions').style.display = 'none';
+}
